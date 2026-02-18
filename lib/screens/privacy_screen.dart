01771cd50dcd9f3390d2ec15a732c30cb5ebb2d5
Colin Kavanagh <colin.sean.kavanagh@gmail.com>
2026-02-18 01:25:01 +0100

    refactor: change Imprint and Privacy screens to use AppHeader widget and delete unnecessary title

--- lib/screens/privacy_screen.dart
+++ lib/screens/privacy_screen.dart
@@ -1,26 +1,21 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/app_header.dart';
+
 class PrivacyScreen extends StatelessWidget {
   const PrivacyScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Datenschutz'),
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      ),
+      appBar: const AppHeader(title: 'Datenschutz'),
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
-              'Datenschutzerklärung',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            Text(
-              'gemäß Art. 13 DSGVO',
+              'Gemäß Art. 13 DSGVO',
               style: Theme.of(context).textTheme.titleMedium,
             ),
             const SizedBox(height: 16),
