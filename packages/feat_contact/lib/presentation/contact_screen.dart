e667f38fdf3e4c8a0d42e26d4cf1e1fe9f2a5081
Felix Busch <fix.busch@gmail.com>
2026-03-06 12:23:47 +0100

    Design rework

--- packages/feat_contact/lib/presentation/contact_screen.dart
+++ packages/feat_contact/lib/presentation/contact_screen.dart
@@ -51,144 +51,208 @@ class _Scaffold extends StatelessWidget {
                 message:
                     'Wenn du dich in einer akuten Krise befindest oder sofort Hilfe benötigst, wende dich bitte an die Telefonseelsorge oder den Notruf.',
               ),
-              Text('Notfall-Kontakte', style: context.textTheme.titleMedium),
-              _ContactCard(
-                title: 'Telefonseelsorge',
-                subtitle: '24/7 kostenlos und vertraulich',
-                actions: [
-                  Flexible(
-                    child: TextButton.icon(
-                      onPressed: () {
-                        final url = Uri(scheme: 'tel', path: '0800 111 0 111');
-                        BlocProvider.of<UrlLauncherBloc>(context).add(
-                          UrlLauncherEvent.onLaunchUrl(url),
-                        );
-                      },
-                      icon: Icon(Icons.phone_outlined),
-                      label: Text('0800 111 0 111'),
-                    ),
-                  ),
-                  Flexible(
-                    child: TextButton.icon(
-                      onPressed: () {
-                        final url = Uri(scheme: 'tel', path: '0800 111 0 222');
-                        BlocProvider.of<UrlLauncherBloc>(context).add(
-                          UrlLauncherEvent.onLaunchUrl(url),
-                        );
-                      },
-                      icon: Icon(Icons.phone_outlined),
-                      label: Text('0800 111 0 222'),
-                    ),
-                  ),
-                ],
-              ),
-              _ContactCard(
-                title: 'Notruf',
-                subtitle: 'Bei akuter Gefahr',
-                actions: [
-                  Expanded(
-                    child: FilledButton.icon(
-                      onPressed: () {
-                        final url = Uri(scheme: 'tel', path: '112');
-                        BlocProvider.of<UrlLauncherBloc>(context).add(
-                          UrlLauncherEvent.onLaunchUrl(url),
-                        );
-                      },
-                      icon: Icon(Icons.phone_outlined),
-                      label: Text('112'),
-                      style: FilledButton.styleFrom(
-                        backgroundColor: context.colors.errorContainer,
-                        foregroundColor: context.colors.onErrorContainer,
+              _ExpandableCard(
+                title: 'Notfall-Kontakte',
+                icon: Icons.emergency_rounded,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    spacing: EikeTheme.verticalComponentSpacingMedium,
+                    children: [
+                      Text(
+                        'Telefonseelsorge',
+                        style: context.textTheme.titleMedium?.copyWith(
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                    ),
+                      Text(
+                        '24 Stunden, 7 Tage die Woche vertrauliche und kostenlose Beratung unter den folgenden Nummern',
+                      ),
+                      Wrap(
+                        spacing: EikeTheme.horizontalComponentSpacingMedium,
+                        children: [
+                          FilledButton.tonalIcon(
+                            onPressed: () {},
+                            icon: Icon(Icons.phone_rounded),
+                            label: Text('0800 111 0 111'),
+                          ),
+                          FilledButton.tonalIcon(
+                            onPressed: () {},
+                            icon: Icon(Icons.phone_rounded),
+                            label: Text('0800 111 0 222'),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    spacing: EikeTheme.verticalComponentSpacingMedium,
+                    children: [
+                      Text(
+                        'Notruf',
+                        style: context.textTheme.titleMedium?.copyWith(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        'Zögern Sie nicht bei akuter Gefahr die Notfalldienste zu benachrichtigen.',
+                      ),
+                      Wrap(
+                        spacing: EikeTheme.horizontalComponentSpacingMedium,
+                        children: [
+                          FilledButton.tonalIcon(
+                            onPressed: () {
+                              BlocProvider.of<UrlLauncherBloc>(context).add(
+                                UrlLauncherEvent.onLaunchUrl(
+                                  Uri(scheme: 'tel', path: '110'),
+                                ),
+                              );
+                            },
+                            icon: Icon(Icons.phone_rounded),
+                            label: Text('110'),
+                          ),
+                          FilledButton.tonalIcon(
+                            onPressed: () {
+                              BlocProvider.of<UrlLauncherBloc>(context).add(
+                                UrlLauncherEvent.onLaunchUrl(
+                                  Uri(scheme: 'tel', path: '112'),
+                                ),
+                              );
+                            },
+                            icon: Icon(Icons.phone_rounded),
+                            label: Text('112'),
+                          ),
+                          FilledButton.tonalIcon(
+                            onPressed: () {
+                              BlocProvider.of<UrlLauncherBloc>(context).add(
+                                UrlLauncherEvent.onLaunchUrl(
+                                  Uri(scheme: 'tel', path: '116117'),
+                                ),
+                              );
+                            },
+                            icon: Icon(Icons.phone_rounded),
+                            label: Text('116117'),
+                          ),
+                        ],
+                      ),
+                    ],
                   ),
                 ],
               ),
-              Text(
-                'PSNV für Einsatzkräfte',
-                style: context.textTheme.titleMedium,
-              ),
-              _ContactCard(
-                title: 'Dein Einsatznachsorgeteam',
-                subtitle:
-                    'Die Kontaktdaten können in den Einstellungen hinterlegt werden.',
-                actions: [
-                  Flexible(
-                    child: FilledButton.icon(
-                      onPressed: phone.buttonAvailable
-                          ? () {
-                              if (phone case TeamContactPhone phone) {
-                                BlocProvider.of<UrlLauncherBloc>(context).add(
-                                  UrlLauncherEvent.onLaunchUrl(
-                                    Uri(scheme: 'tel', path: phone),
-                                  ),
-                                );
-                              }
-                            }
-                          : null,
-                      icon: Icon(Icons.phone_outlined),
-                      label: Text(phone.buttonText),
-                    ),
-                  ),
-                  Flexible(
-                    child: FilledButton.icon(
-                      onPressed: mail.buttonAvailable
-                          ? () {
-                              if (mail case TeamContactEmail mail) {
-                                BlocProvider.of<UrlLauncherBloc>(context).add(
-                                  UrlLauncherEvent.onLaunchUrl(
-                                    Uri(scheme: 'mailto', path: mail),
-                                  ),
-                                );
-                              }
-                            }
-                          : null,
-                      icon: Icon(Icons.mail_outline),
-                      label: Text(mail.buttonText),
-                    ),
+              _ExpandableCard(
+                title: 'PSNV für Einsatzkräfte',
+                icon: Icons.contact_emergency_rounded,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    spacing: EikeTheme.verticalComponentSpacingMedium,
+                    children: [
+                      Text(
+                        'Dein Einsatznachsorgeteam',
+                        style: context.textTheme.titleMedium?.copyWith(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        'Die Kontaktdaten können in den Einstellungen hinterlegt werden.',
+                      ),
+                      Wrap(
+                        spacing: EikeTheme.horizontalComponentSpacingMedium,
+                        children: [
+                          if (phone case TeamContactPhone phone)
+                            if (phone.isNotEmpty)
+                              FilledButton.icon(
+                                onPressed: () {
+                                  BlocProvider.of<UrlLauncherBloc>(context).add(
+                                    UrlLauncherEvent.onLaunchUrl(
+                                      Uri(scheme: 'tel', path: phone),
+                                    ),
+                                  );
+                                },
+                                icon: Icon(Icons.phone_outlined),
+                                label: Text(phone),
+                              ),
+                          if (mail case TeamContactEmail mail)
+                            if (mail.isNotEmpty)
+                              FilledButton.icon(
+                                onPressed: () {
+                                  BlocProvider.of<UrlLauncherBloc>(context).add(
+                                    UrlLauncherEvent.onLaunchUrl(
+                                      Uri(scheme: 'mailto', path: mail),
+                                    ),
+                                  );
+                                },
+                                icon: Icon(Icons.mail_outline),
+                                label: Text(mail),
+                              ),
+                        ],
+                      ),
+                    ],
                   ),
                 ],
               ),
-              // StreamBuilder<TeamContact?>(
-              //   stream: context.read<AppDatabase>().watchTeamContact(),
-              //   builder: (context, snapshot) {
-              //     final team = snapshot.data;
-              //
-              //     final phone = (team?.phone ?? '').trim();
-              //     final email = (team?.email ?? '').trim();
-              //
-              //     final hasPhone = phone.isNotEmpty;
-              //     final hasEmail = email.isNotEmpty;
-              //
-              //     return _ContactCard(
-              //       title: 'Dein Einsatznachsorgeteam',
-              //       subtitle:
-              //           'Die Kontaktdaten können in den Einstellungen hinterlegt werden.',
-              //       actions: [
-              //         FilledButton.icon(
-              //           onPressed: hasPhone
-              //               ? () => _dialPhone(context, phone)
-              //               : null,
-              //           icon: Icon(Icons.phone_outlined),
-              //           label: hasPhone ? phone : 'Nicht hinterlegt',
-              //         ),
-              //         FilledButton.icon(
-              //           onPressed: hasEmail
-              //               ? () => _sendEmail(
-              //                   context,
-              //                   email,
-              //                   subject: 'EIKE – Einsatznachsorge',
-              //                 )
-              //               : null,
-              //           icon: Icon(Icons.mail_outline),
-              //           label: hasEmail ? email : 'Nicht hinterlegt',
-              //         ),
-              //       ],
-              //     );
-              //   },
-              // ),
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class _ExpandableCard extends StatelessWidget {
+  const _ExpandableCard({
+    required this.title,
+    required this.icon,
+    required this.children,
+  });
+
+  final String title;
+  final IconData icon;
+  final List<Widget> children;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      clipBehavior: Clip.antiAlias,
+      child: Theme(
+        data: Theme.of(context).copyWith(dividerColor: Colors.transparent),
+        child: ExpansionTile(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(EikeTheme.cornerRadius),
+          ),
+          title: Row(
+            spacing: EikeTheme.horizontalComponentSpacingMedium,
+            children: [
+              Icon(icon),
+              Text(title),
+            ],
+          ),
+          initiallyExpanded: true,
+          expandedCrossAxisAlignment: CrossAxisAlignment.start,
+          childrenPadding: EdgeInsets.only(
+            left: EikeTheme.horizontalCardPadding,
+            right: EikeTheme.horizontalCardPadding,
+            bottom: EikeTheme.verticalCardPadding,
+          ),
+          children: [
+            if (children.isNotEmpty) ...[
+              Divider(
+                color: context.colors.surfaceContainerHighest,
+                height: 2,
+                thickness: 2,
+                indent: 0,
+              ),
+              const SizedBox(height: EikeTheme.verticalComponentSpacingMedium),
+            ],
+            for (final (index, child) in children.indexed) ...[
+              if (index > 0)
+                const SizedBox(
+                  height: EikeTheme.verticalComponentSpacingMedium,
+                ),
+              child,
+            ],
+          ],
         ),
       ),
     );
@@ -236,55 +300,4 @@ class _AlertCard extends StatelessWidget {
       ),
     );
   }
-}
-
-class _ContactCard extends StatelessWidget {
-  const _ContactCard({
-    required this.title,
-    required this.subtitle,
-    required this.actions,
-  });
-
-  final String title;
-  final String subtitle;
-  final List<Widget> actions;
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      child: Padding(
-        padding: EikeTheme.cardPadding,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          spacing: EikeTheme.verticalComponentSpacingMedium,
-          children: [
-            Text(
-              title,
-              style: context.textTheme.titleMedium?.copyWith(
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Text(subtitle),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              spacing: EikeTheme.horizontalComponentSpacingMedium,
-              children: actions,
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-extension on TeamContactPhone? {
-  String get buttonText =>
-      this?.isNotEmpty == true ? toString() : 'Nicht hinterlegt';
-  bool get buttonAvailable => this?.isNotEmpty == true;
-}
-
-extension on TeamContactEmail? {
-  String get buttonText =>
-      this?.isNotEmpty == true ? toString() : 'Nicht hinterlegt';
-  bool get buttonAvailable => this?.isNotEmpty == true;
 }
