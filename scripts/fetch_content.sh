f9d4b334f9373b0b169cb17c2dd0c0f29a9c0519
Felix Busch <fix.busch@gmail.com>
2026-06-09 18:27:00 +0200

    fetch content ist toll

--- scripts/fetch_content.sh
+++ scripts/fetch_content.sh
@@ -14,6 +14,8 @@ ZIP_FILE="content.zip"
 EXTRACTED_DIR="eike-content-${VERSION}"
 TARGET_DIR="../packages/eike_app/assets/content"
 
+rm -rf ${TARGET_DIR}/
+
 echo "Downloading version ${VERSION} ..."
 curl -L "https://github.com/EikeApp/eike-content/archive/refs/tags/${VERSION}.zip" -o "${ZIP_FILE}"
 
@@ -25,27 +27,9 @@ mkdir -p eike_content
 mv "${EXTRACTED_DIR}/7-things" eike_content/
 mv "${EXTRACTED_DIR}/data.yaml" eike_content/
 
-#
-# # if command -v yq &> /dev/null; then
-# #   echo "Converting YAML to JSON"
-# #   yq -o=json eike_content/data.yaml > eike_content/data.json
-# #   rm -f eike_content/data.yaml
-# # else
-# #   echo "Could not convert YAML to JSON due to missing dependency on yq."
-# #   echo "Consider installing yq using Homebrew for example."
-# #   exit 1
-# # fi
-#
-# echo "Cleaning old content ..."
-# rm -rf "${TARGET_DIR}"
-# mkdir -p "${TARGET_DIR}"
-#
-# echo "Moving new content"
-# mv eike_content/* "${TARGET_DIR}/"
-# mv eike_content/.version "${TARGET_DIR}/"
-# # rmdir eike_content
-#
-# echo "Cleaning up ..."
-# # rm -rf "${ZIP_FILE}" "${EXTRACTED_DIR}"
-#
-# echo "Done!"
+mkdir -p "${TARGET_DIR}"
+mv eike_content/* ${TARGET_DIR}/
+
+rm -rf eike_content/
+rm -rf "${EXTRACTED_DIR}"
+rm "${ZIP_FILE}"
