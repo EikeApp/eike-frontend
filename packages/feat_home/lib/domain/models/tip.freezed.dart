817b26c46da7c4a77fb821724df76ce151a361a6
Felix Busch <fix.busch@gmail.com>
2026-08-28 14:03:45 +0200

    pump up dart+flutter version

--- packages/feat_home/lib/domain/models/tip.freezed.dart
+++ packages/feat_home/lib/domain/models/tip.freezed.dart
@@ -1,6 +1,6 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 // coverage:ignore-file
-// ignore_for_file: type=lint
+// ignore_for_file: type=lint, type=warning, deprecated_member_use, deprecated_member_use_from_same_package
 // ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
 
 part of 'tip.dart';
@@ -9,6 +9,7 @@ part of 'tip.dart';
 // FreezedGenerator
 // **************************************************************************
 
+// GENERATED CODE - DO NOT MODIFY BY HAND
 // dart format off
 T _$identity<T>(T value) => value;
 
@@ -66,7 +67,7 @@ class _$TipImageCopyWithImpl<$Res>
 /// Create a copy of TipImage
 /// with the given fields replaced by the non-null parameter values.
 @pragma('vm:prefer-inline') @override $Res call({Object? imagePath = null,Object? alt = null,}) {
-  return _then(_self.copyWith(
+  return _then(TipImage(
 imagePath: null == imagePath ? _self.imagePath : imagePath // ignore: cast_nullable_to_non_nullable
 as String,alt: null == alt ? _self.alt : alt // ignore: cast_nullable_to_non_nullable
 as String,
@@ -326,7 +327,7 @@ class _$TipCopyWithImpl<$Res>
 /// Create a copy of Tip
 /// with the given fields replaced by the non-null parameter values.
 @pragma('vm:prefer-inline') @override $Res call({Object? id = null,Object? position = null,Object? title = null,Object? description = null,Object? image = null,}) {
-  return _then(_self.copyWith(
+  return _then(Tip(
 id: null == id ? _self.id : id // ignore: cast_nullable_to_non_nullable
 as int,position: null == position ? _self.position : position // ignore: cast_nullable_to_non_nullable
 as int,title: null == title ? _self.title : title // ignore: cast_nullable_to_non_nullable
--- packages/feat_notification/lib/presentation/bloc/notification_bloc.freezed.dart
+++ packages/feat_notification/lib/presentation/bloc/notification_bloc.freezed.dart
@@ -1,6 +1,6 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 // coverage:ignore-file
-// ignore_for_file: type=lint
+// ignore_for_file: type=lint, type=warning, deprecated_member_use, deprecated_member_use_from_same_package
 // ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
 
 part of 'notification_bloc.dart';
@@ -9,6 +9,7 @@ part of 'notification_bloc.dart';
 // FreezedGenerator
 // **************************************************************************
 
+// GENERATED CODE - DO NOT MODIFY BY HAND
 // dart format off
 T _$identity<T>(T value) => value;
 /// @nodoc
@@ -549,7 +550,7 @@ class _$NotificationStateCopyWithImpl<$Res>
 /// Create a copy of NotificationState
 /// with the given fields replaced by the non-null parameter values.
 @pragma('vm:prefer-inline') @override $Res call({Object? notifications = null,Object? currentNotificationIndex = null,}) {
-  return _then(_self.copyWith(
+  return _then(NotificationState(
 notifications: null == notifications ? _self.notifications : notifications // ignore: cast_nullable_to_non_nullable
 as List<EikeNotification>,currentNotificationIndex: null == currentNotificationIndex ? _self.currentNotificationIndex : currentNotificationIndex // ignore: cast_nullable_to_non_nullable
 as int,
@@ -687,7 +688,7 @@ return $default(_that.notifications,_that.currentNotificationIndex);case _:
 
 
 class _NotificationState implements NotificationState {
-  const _NotificationState({required final  List<EikeNotification> notifications, required this.currentNotificationIndex}): _notifications = notifications;
+  const _NotificationState({required  List<EikeNotification> notifications, required this.currentNotificationIndex}): _notifications = notifications;
   
 
  final  List<EikeNotification> _notifications;
