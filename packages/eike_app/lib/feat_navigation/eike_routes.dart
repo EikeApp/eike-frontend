8886da615108968e0d57a06ae4d3de3b26ac72f6
Felix Busch <fix.busch@gmail.com>
2026-08-29 16:57:58 +0200

    go router shells integration

--- packages/eike_app/lib/feat_navigation/eike_routes.dart
+++ packages/eike_app/lib/feat_navigation/eike_routes.dart
@@ -1,12 +1,8 @@
 enum EikeRoute {
-  home('/home'),
-  contact('/contact'),
-  settings('/settings'),
-  imprint('/imprint'),
-  licenses('/licenses'),
-  privacyPolicy('/privacyPolicy')
-  ;
-
-  const EikeRoute(this.route);
-  final String route;
+  home,
+  contact,
+  settings,
+  imprint,
+  licenses,
+  privacyPolicy,
 }
